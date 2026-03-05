e2ca32f97e41116f6763da404c6f6d8e29c12e75
JayaniSrimali <jayanisrimali666@gmail.com>
2026-03-05 16:55:55 +0530

    chore(backend): adjust server configurations

--- serendib-backend/server.js
+++ serendib-backend/server.js
@@ -20,7 +20,7 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5174"], credentials: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
